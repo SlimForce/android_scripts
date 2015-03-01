97b993b9b9707e55e61a032adb5ab7172906f455
Chang Long <changlon@gmail.com>
2015-03-01 10:46:54 +0100

    add local_manifests

--- bin/merge.sh
+++ bin/merge.sh
@@ -28,6 +28,10 @@ cd $work_dir && echo -e "\n===> $project: " && cd $project && git fetch $remote_
 project=vendor_slim
 cd $work_dir && echo -e "\n===> $project: " && cd $project && git fetch $remote_origin && git fetch $remote_name && git merge $remote_name/$branch_name && git status
 
+branch_name=cm12.0
+project=proprietary_vendor_lge
+cd $work_dir && echo -e "\n===> $project: " && cd $project && git fetch $remote_origin && git fetch $remote_name && git merge $remote_name/$branch_name && git status
+
 exit;
 
 project=frameworks_opt_telephony
