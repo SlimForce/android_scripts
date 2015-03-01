670091bbdaebea37bc11a6a24a943c48060bf8ee
Chang Long <changlon@gmail.com>
2015-03-01 09:33:30 +0100

    update merge

--- bin/merge.sh
+++ bin/merge.sh
@@ -1,38 +1,37 @@
 #!/usr/bin/env bash
 
-
 work_dir=`pwd`
+remote_origin=origin
 remote_name=slimsaber
 owner_name=SlimSaber
 branch_name=lp5.0-caf
 
 project=packages_apps_Settings
-cd $work_dir && echo "$project -> $remote_name" && cd $project && git fetch $remote_name && git merge $remote_name/$branch_name
+cd $work_dir && echo -e "\n===> $project: " && cd $project && git fetch $remote_origin && git fetch $remote_name && git merge $remote_name/$branch_name && git status
 
 project=packages_services_Telephony
-cd $work_dir && echo "$project -> $remote_name" && cd $project && git fetch $remote_name && git merge $remote_name/$branch_name
+cd $work_dir && echo -e "\n===> $project: " && cd $project && git fetch $remote_origin && git fetch $remote_name && git merge $remote_name/$branch_name && git status
 
 project=frameworks_base
-cd $work_dir && echo "$project -> $remote_name" && cd $project && git fetch $remote_name && git merge $remote_name/$branch_name
+cd $work_dir && echo -e "\n===> $project: " && cd $project && git fetch $remote_origin && git fetch $remote_name && git merge $remote_name/$branch_name && git status
 
 project=packages_apps_InCallUI
-cd $work_dir && echo "$project -> $remote_name" && cd $project && git fetch $remote_name && git merge $remote_name/$branch_name
+cd $work_dir && echo -e "\n===> $project: " && cd $project && git fetch $remote_origin && git fetch $remote_name && git merge $remote_name/$branch_name && git status
 
 project=packages_apps_Dialer
-cd $work_dir && echo "$project -> $remote_name" && cd $project && git fetch $remote_name && git merge $remote_name/$branch_name
+cd $work_dir && echo -e "\n===> $project: " && cd $project && git fetch $remote_origin && git fetch $remote_name && git merge $remote_name/$branch_name && git status
 
 branch_name=lp5.0
 project=android_build
-cd $work_dir && echo "$project -> $remote_name" && cd $project && git fetch $remote_name && git merge $remote_name/$branch_name
+cd $work_dir && echo -e "\n===> $project: " && cd $project && git fetch $remote_origin && git fetch $remote_name && git merge $remote_name/$branch_name && git status
 
 project=vendor_slim
-cd $work_dir && echo "$project -> $remote_name" && cd $project && git fetch $remote_name && git merge $remote_name/$branch_name
-
+cd $work_dir && echo -e "\n===> $project: " && cd $project && git fetch $remote_origin && git fetch $remote_name && git merge $remote_name/$branch_name && git status
 
 exit;
 
 project=frameworks_opt_telephony
-cd $work_dir && echo "$project -> $remote_name" && cd $project && git fetch $remote_name && git merge $remote_name/$branch_name
+cd $work_dir && echo -e "\n===> $project: " && cd $project && git fetch $remote_origin && git fetch $remote_name && git merge $remote_name/$branch_name && git status
 
 project=device_lge_hammerheadcaf
-cd $work_dir && echo "$project -> $remote_name" && cd $project && git fetch $remote_name && git merge $remote_name/$branch_name
+cd $work_dir && echo -e "\n===> $project: " && cd $project && git fetch $remote_origin && git fetch $remote_name && git merge $remote_name/$branch_name && git status
