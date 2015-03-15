68ea37ce515a78fbc1b6cc4e2102a03ed37309c5
Chang Long <changlon@gmail.com>
2015-03-15 05:24:15 +0100

    add signapk.jar

--- bin/sign.sh
+++ bin/sign.sh
@@ -5,19 +5,23 @@ cd `dirname $0`
 my_bash_script_path=`pwd -P`
 cd $my_bash_work_path
 
-jar_file="$my_bash_script_path/../jars/testsign.jar"
+lib="$my_bash_script_path/../jars"
 
 zip_file="$1"
+file_name=$(basename "$zip_file")
+file_name=${file_name%.zip}
+file_dir=$(dirname "$zip_file")
+file_signed="$file_dir/${file_name}_signed.zip"
 
 if [ ! -e $zip_file ]; then
         if [[ "$zip_file" == *.zip ]]; then
                 echo "zip -r $@"
-                zip -r $@ && java -classpath $jar_file testsign $zip_file
+                zip -r $@ && echo "sign $zip_file -> $file_signed" && java -jar "$lib/signapk.jar" "$lib/sign_cert.pem" "$lib/sign_key.pk8" $zip_file $file_signed
         else
                 echo "usage: $0 path/file.zip *"
         fi
         exit;
 fi
 
-java -classpath $jar_file testsign $zip_file
-
+echo "sign $zip_file -> $file_signed"
+java -jar "$lib/signapk.jar" "$lib/sign_cert.pem" "$lib/sign_key.pk8" $zip_file $file_signed
